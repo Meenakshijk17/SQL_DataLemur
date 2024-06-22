dc0f7d711983115635d175ce378614a4ee97f6ec
Meenakshi Krishnakumar <63089206+Meenakshijk17@users.noreply.github.com>
2024-06-22 02:08:19 -0400

    Update Average Review Ratings [Amazon SQL Interview Question].sql

--- Average Review Ratings [Amazon SQL Interview Question].sql
+++ Average Review Ratings [Amazon SQL Interview Question].sql
@@ -1,3 +1,5 @@
+-- https://datalemur.com/questions/sql-avg-review-ratings
+
 -- Given the reviews table, write a query to retrieve the average star rating for each product, grouped by month. The output should display the month as a numerical value, product ID, and average star rating rounded to two decimal places. Sort the output first by month and then by product ID.
 
 -- reviews Table:
